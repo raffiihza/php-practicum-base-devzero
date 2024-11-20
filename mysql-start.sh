ef2677bdb2aa4d76b9efadafa18345e8fd0f4c28
Raffi Ihza Zuhairnawan <>
2024-11-20 03:47:37 +0000

    Not needed

--- mysql-start.sh
+++
@@ -1,9 +0,0 @@
-sudo docker run -d \
-  --name db \
-  --restart unless-stopped \
-  -v mariadb-data:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=root \
-  -e MYSQL_DATABASE=mariadb \
-  -e MYSQL_USER=mariadb \
-  -e MYSQL_PASSWORD=mariadb \
-  mariadb:10.4
